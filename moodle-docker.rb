6b161ba1569a5dc4e53daa5253aab6179f44e0f0
Vincent Cornelis <vincent.cornelis@ldesignmedia.nl>
2023-05-03 18:19:21 +0200

    Update to v0.0.5

--- moodle-docker.rb
+++ moodle-docker.rb
@@ -1,8 +1,8 @@
 class MoodleDocker < Formula
   desc "Moodle Docker"
   homepage "https://www.ldesignmedia.nl"
-  url "https://github.com/LdesignMedia/moodle-docker-brew/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "dde8d011ae88a9d70d5121afda77d4c0649a4f47128f75fec18ecc7c0f60f1b8"
+  url "https://github.com/LdesignMedia/moodle-docker-brew/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "4e57d5eae274e54e909eeac80630fe7a2b6859fe6f78f9a0ae2bf04e41bfeb4a"
   license ""
 
   depends_on "bash"
@@ -22,8 +22,6 @@ class MoodleDocker < Formula
 
   def post_install
     system "git", "clone", "https://github.com/moodlehq/moodle-docker.git", "#{bin}/moodlehq-docker"
-    cd "#{bin}/moodlehq-docker" do
-      system "git", "checkout", "cbea8b8a657a28dae2b036b25046d332fb415ee2"
     end
   end
 end

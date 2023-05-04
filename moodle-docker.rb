1dd8e36dd376366a5b8f785892393c19df4f306c
Vincent Cornelis <vincent.cornelis@ldesignmedia.nl>
2023-05-04 11:37:49 +0200

    Update to v0.0.11

--- moodle-docker.rb
+++ moodle-docker.rb
@@ -18,9 +18,11 @@ class MoodleDocker < Formula
     bin.install Dir["scripts"]
     prefix.install "README.md"
     prefix.install "moodle_versions.txt"
+    prefix.instal Dir["moodlehq-docker"]
   end
 
   def post_install
     system "git", "clone", "https://github.com/moodlehq/moodle-docker.git", "#{bin}/moodlehq-docker"
+    system "git", "clone", "https://github.com/moodlehq/moodle-docker.git", "#{prefix}/moodlehq-docker"
   end
 end

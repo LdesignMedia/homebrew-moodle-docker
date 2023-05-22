c26f722761209666494086ae9853013dd5311d48
Luuk Verhoeven <mail@ldesign.nl>
2023-05-22 17:26:54 +0200

    Update moodle-docker.rb

--- moodle-docker.rb
+++ moodle-docker.rb
@@ -1,9 +1,9 @@
 class MoodleDocker < Formula
   desc "Moodle Docker"
   homepage "https://ldesignmedia.nl"
-  url "https://github.com/LdesignMedia/homebrew-moodle-docker/archive/refs/tags/v1.0.0.zip"
+  url "https://github.com/LdesignMedia/homebrew-moodle-docker/archive/refs/tags/v1.0.1.zip"
   sha256 "9a2867cf395a49c326c1846c6c029889f6d6714cad52af30a60059e80ac60e91"
-  version "1.0.3"
+  version "1.0.1"
   license ""
 
   depends_on "bash"

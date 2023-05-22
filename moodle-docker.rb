bac86f55866e316c7c6308d1c1a3341d51d56ee9
Luuk Verhoeven <mail@ldesign.nl>
2023-05-22 14:37:07 +0200

    Update moodle-docker.rb

--- moodle-docker.rb
+++ moodle-docker.rb
@@ -3,6 +3,7 @@ class MoodleDocker < Formula
   homepage "https://www.ldesignmedia.nl"
   url "https://github.com/LdesignMedia/moodle-docker-brew/archive/refs/tags/v0.0.11.tar.gz"
   sha256 "f4a377844cb26947050b65a2a1dee2f48e2744b133fdb5f00e6244e76dff0ea9"
+  version "1.0.0"
   license ""
 
   depends_on "bash"
@@ -12,15 +13,11 @@ class MoodleDocker < Formula
   depends_on "unzip"
 
   def install
-    bin.install "moodle-docker"
-    bin.install Dir["moodle"]
-    bin.install Dir["moodlehq-docker"]
-    bin.install Dir["scripts"]
-    prefix.install "README.md"
-    prefix.install "moodle_versions.txt"
+    bin.install "#{ENV["HOME"]}/.moodle-docker-brew/install.sh"
   end
 
   def post_install
-    system "git", "clone", "https://github.com/moodlehq/moodle-docker.git", "#{bin}/moodlehq-docker"
+    system "git", "clone", "https://github.com/LdesignMedia/moodle-docker-brew", "#{ENV["HOME"]}/.moodle-docker-brew"
   end
+  
 end

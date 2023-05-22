5d4442272a0bc3a5880d8959751a83375362030f
Luuk Verhoeven <luuk@ldesign.nl>
2023-05-22 21:43:33 +0200

    Update

--- moodle-docker.rb
+++ moodle-docker.rb
@@ -2,7 +2,7 @@ class MoodleDocker < Formula
   desc "Moodle Docker"
   homepage "https://ldesignmedia.nl"
   url "https://github.com/LdesignMedia/homebrew-moodle-docker.git", :branch => "main", :using => :git
-  version "1.0.10"
+  version "1.0.11"
 
   depends_on "bash"
   depends_on "wget"

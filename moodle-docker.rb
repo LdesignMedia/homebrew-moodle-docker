320da8f3daf67dcbae83814789341e9dca0e3333
Luuk Verhoeven <luuk@ldesign.nl>
2024-04-26 09:37:32 +0200

    Update version number try if we can force update the source with this change

--- moodle-docker.rb
+++ moodle-docker.rb
@@ -2,7 +2,7 @@ class MoodleDocker < Formula
   desc "Moodle Docker"
   homepage "https://ldesignmedia.nl"
   url "https://github.com/LdesignMedia/homebrew-moodle-docker.git", :branch => "main", :using => :git
-  version "1.0.15"
+  version "1.0.17"
 
   depends_on "bash"
   depends_on "wget"
